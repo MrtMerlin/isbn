907a02100b1559af3168c2519c8c9539bfbc9474
Martin Lewis <mrtmerlin@gmail.com>
2021-04-02 19:01:07 +0100

    bookcontroller update.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,15 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 use App\Models\Book;
+use Illuminate\Support\Facades\Http;
 
 class BookController extends Controller
 {
 
+    public function showBook()
+    {
+        return route('showBook');
+    }
+
     public function fetchBook(Request $request)
     {
-        $reponse = Http::get('https://openlibrary.org/api/' . $request->input());
+        $num = $request->input('search');
+
+        $book = Http::withHeaders([
+                        'accept' => 'application/json'
+                      ])
+                        ->get('https://openlibrary.org/isbn/'.$num)
+                        ->json();
+
+        $description = Http::withHeaders([
+                            'accept' => 'application/json'
+                        ])
+                        ->get('https://openlibrary.org'.$book['works'][0]['key'].'.json')
+                        ->json();
+        $author = Http::withHeaders([
+                        'accept' => 'application/json'
+                    ])
+                    ->get('https://openlibrary.org'.$book['authors'][0]['key'])
+                    ->json();
+
+        $returnedCollection = array(
+            'title' => $book['title'],
+            'description' => $description['description'],
+            'author' => $author['name'],
+        );
+
+        return view('showBook')->with(compact('returnedCollection'));
+
     }
 
 }
